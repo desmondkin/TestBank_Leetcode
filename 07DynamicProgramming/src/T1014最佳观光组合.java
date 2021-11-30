579e2800a7e19f13c5c363303384083eb05e95af
Desmond <Desmond@git.com>
2021-11-30 20:32:55 +0800

    version 0.11 for leetcode working on 07DP

--- 07DynamicProgramming/src/T1014最佳观光组合.java
+++ 07DynamicProgramming/src/T1014最佳观光组合.java
@@ -1,2 +1,10 @@
 public class T1014最佳观光组合 {
+    public int maxScoreSightseeingPair(int[] values) {
+        int res = 0, mx = values[0];
+        for (int i = 1; i < values.length; ++i) {
+            res = Math.max(res, values[i] - i + mx);
+            mx = Math.max(mx, values[i] + i);
+        }
+        return res;
+    }
 }
